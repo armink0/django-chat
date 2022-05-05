8e9837c6886e04e32d2f50e0f2cd25976b2a2413
Armin Kocanovic <armin.kocanovic@student.pmf.unibl.org>
2022-05-05 15:22:24 +0200

    Dodavanje pretrage i pocetni izgled chat sobe.

--- chat/settings.py
+++ chat/settings.py
@@ -125,3 +125,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ASGI_APPLICATION = "chat.asgi.application"
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
+}
--- chat_app/views.py
+++ chat_app/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'chat/index.html')
+
+def soba(request, naziv_sobe):
+    return render(request, 'chat/soba.html', {
+        'naziv_sobe': naziv_sobe
+    })
